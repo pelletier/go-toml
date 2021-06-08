8360726914423965f83fbf67b325056ef0168d14
Nabetani <takenori@nabetani.sakura.ne.jp>
2021-06-08 20:17:46 +0900

    test umarshal recursive table array

--- recursive_table_test.go
+++ recursive_table_test.go
@@ -79,3 +79,94 @@ func TestUnmarshal_RecursiveTable(t *testing.T) {
 		})
 	}
 }
+
+func TestUnmarshal_RecursiveTableArray(t *testing.T) {
+	type Foo struct {
+		I int
+		F []*Foo
+	}
+
+	examples := []struct {
+		desc     string
+		input    string
+		expected string
+		err      bool
+	}{
+		{
+			desc: "simplest",
+			input: `
+				I=1
+				F=[]
+			`,
+			expected: `{"I":1,"F":[]}`,
+		},
+		{
+			desc: "depth 1",
+			input: `
+				I=1
+				[[F]]
+				I=2
+				F=[]
+			`,
+			expected: `{"I":1,"F":[{"I":2,"F":[]}]}`,
+		},
+		{
+			desc: "depth 2",
+			input: `
+				I=1
+				[[F]]
+				I=2
+				[[F.F]]
+				I=3
+				F=[]
+			`,
+			expected: `{"I":1,"F":[{"I":2,"F":[{"I":3,"F":[]}]}]}`,
+		},
+		{
+			desc: "depth 3",
+			input: `
+				I=1
+				[[F]]
+				I=2
+				[[F.F]]
+				I=3
+				[[F.F.F]]
+				I=4
+				F=[]
+			`,
+			expected: `{"I":1,"F":[{"I":2,"F":[{"I":3,"F":[{"I":4,"F":[]}]}]}]}`,
+		},
+		{
+			desc: "depth 4",
+			input: `
+				I=1
+				[[F]]
+				I=2
+				[[F.F]]
+				I=3
+				[[F.F.F]]
+				I=4
+				[[F.F.F.F]]
+				I=5
+				F=[]
+			`,
+			expected: `{"I":1,"F":[{"I":2,"F":[{"I":3,"F":[{"I":4,"F":[{"I":5,"F":[]}]}]}]}]}`,
+		},
+	}
+
+	for _, ex := range examples {
+		e := ex
+		t.Run(e.desc, func(t *testing.T) {
+			foo := Foo{}
+			err := toml.Unmarshal([]byte(e.input), &foo)
+			if e.err {
+				require.Error(t, err)
+			} else {
+				require.NoError(t, err)
+				j, err := json.Marshal(foo)
+				require.NoError(t, err)
+				assert.Equal(t, e.expected, string(j))
+			}
+		})
+	}
+}
