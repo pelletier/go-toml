0cca8d325b6a09ee6071879de6bcc5def8e397ae
eNV25 <env252525@gmail.com>
2022-09-10 21:50:30 +0530

    fixup! fixup! Fix reflect.*Header in TinyGo.

--- internal/danger/sizetype.go
+++ internal/danger/sizetype.go
@@ -2,4 +2,5 @@
 
 package danger
 
+// https://github.com/golang/go/blob/54182ff/src/reflect/value.go#L2760
 type sizeType = int
--- internal/danger/sizetype_tinygo.go
+++ internal/danger/sizetype_tinygo.go
@@ -2,4 +2,5 @@
 
 package danger
 
+// https://github.com/tinygo-org/tinygo/blob/f0391ea/src/reflect/value.go#L773
 type sizeType = uintptr
