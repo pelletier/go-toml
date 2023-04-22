4be788c1ee5860c535f54175f25b89624d28bfc8
manunio <max.manu.nair@gmail.com>
2023-04-22 23:37:31 +0530

    fuzz: move fuzz_target from oss-fuzz
    - This pr attempts to move `fuzz_toml` fuzz_target from oss-fuzz to go-toml.
    - Its recommended by oss-fuzz to move fuzz-target upstream as this eases maintenance of fuzz_target.
    - It also include check against too large input.

---
+++ oss_fuzz.go
@@ -0,0 +1,43 @@
+//go:build go1.18 || go1.19 || go1.20
+// +build go1.18 go1.19 go1.20
+
+package toml
+
+import (
+	"fmt"
+	"reflect"
+	"strings"
+)
+
+func FuzzToml(data []byte) int {
+	if len(data) >= 10240 {
+		return 0
+	}
+
+	if strings.Contains(string(data), "nan") {
+		return 0
+	}
+
+	var v interface{}
+	err := Unmarshal(data, &v)
+	if err != nil {
+		return 0
+	}
+
+	encoded, err := Marshal(v)
+	if err != nil {
+		return 0
+	}
+
+	var v2 interface{}
+	err = Unmarshal(encoded, &v2)
+	if err != nil {
+		panic(fmt.Sprintf("Failed round trip: %s", err))
+	}
+
+	if !reflect.DeepEqual(v, v2) {
+		panic(fmt.Sprintf("Not equal: %#+v %#+v", v, v2))
+	}
+
+	return 1
+}
