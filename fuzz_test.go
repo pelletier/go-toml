2b69615b5d5552a1b59c1685cad0e0e662c741f4
Thomas Pelletier <thomas@pelletier.codes>
2024-02-27 15:30:13 -0500

    Go 1.22 support (#935)

--- fuzz_test.go
+++ fuzz_test.go
@@ -1,5 +1,5 @@
-//go:build go1.18 || go1.19 || go1.20 || go1.21
-// +build go1.18 go1.19 go1.20 go1.21
+//go:build go1.18 || go1.19 || go1.20 || go1.21 || go1.22
+// +build go1.18 go1.19 go1.20 go1.21 go1.22
 
 package toml_test
 
